b486f00a788b630635b7dd7bdf69acb923e7c5d6
lm902 <i@lm902.cn>
2020-03-11 14:38:49 -0700

    Project init

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript'
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'bundle.js',
+    format: 'umd'
+  },
+  plugins: [typescript()],
+  external: []
+}
